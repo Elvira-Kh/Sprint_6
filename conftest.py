051262b51317c4526bab871289ecd225934fc2b6
Elvira <138506199+Elvira-Kh@users.noreply.github.com>
2024-04-04 17:27:44 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,10 +1,1 @@
-from selenium import webdriver
-import pytest
 
-@pytest.fixture()
-def driver():
-    driver = webdriver.Firefox()
-    driver.implicitly_wait(10)
-    driver.get('https://qa-scooter.praktikum-services.ru/')
-    yield driver
-    driver.quit()
